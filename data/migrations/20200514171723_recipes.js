e9069a93aa126b1cdf9425e5cd5d3baaebe3d2f1
Julia Vu <juliadvu@gmail.com>
2020-05-14 17:56:16 -0500

    tables created

---
+++ data/migrations/20200514171723_recipes.js
@@ -0,0 +1,43 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable("recipes", tbl=>{
+      tbl.increments()
+      tbl.string("recipe-name", 256).notNullable().index();
+      tbl.string("instructions", 255).notNullable();
+  })
+
+  .createTable("ingredients",tbl=>{
+      tbl.increments();
+      tbl.string("ingredient-name").notNullable().unique().index()
+      tbl.float("quantity").notNullable();
+      tbl.integer("recipe_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("recipes")
+        .onDelete("RESTRIC")
+        .onUpdate("CASCADE")
+  })
+  .createTable('steps', tbl=>{
+      tbl.increments();
+      tbl.integer("step_number").unsigned().notNullable()
+      tbl.string("instructions").notNullable()
+      tbl.integer("recipe_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("recipes")
+        .onDelete("RESTRIC")
+        .onUpdate("CASCADE")
+
+  })
+
+
+};//end of exports.up
+
+exports.down = function(knex) {
+  return knex.schema
+  .dropTableIfExists("steps")
+  .dropTableIfExists("ingredients")
+  .dropTableIfExists("recipes")
+};
