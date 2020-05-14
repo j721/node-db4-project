675816b223dd2a7a305017e7e95fd271c5c79dfe
Julia Vu <juliadvu@gmail.com>
2020-05-14 16:26:22 -0500

    added in pool for foreign key restriction into knexfile

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ module.exports = {
       client: 'sqlite3',
       useNullAsDefault: true, // needed for sqlite
       connection: {
-        filename: './data/zoos.db3',
+        filename: './data/recipes.db3',
       },
       migrations: {
         directory: './data/migrations'
@@ -11,6 +11,11 @@ module.exports = {
       seeds: {
         directory: './data/seeds'
       },
+      pool: {
+        afterCreate: (conn, done) => {
+          // runs after a connection is made to the sqlite engine
+          conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+        }
     },
-  };
-  
+  }
+}
